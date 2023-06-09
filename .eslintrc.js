fc1a2961ba094b285372344a7b862a8fbacb017a
Harsh <rafftar98@gmail.com>
2023-06-09 22:26:20 +0530

    init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "standard-with-typescript",
+    "plugin:react/recommended",
+    "google",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+  settings: {
+    react: {
+      version: "latest",
+    },
+  },
+};
---
+++ components/Navbar.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="">
+      <div>
+        <Link href="/">
+          <span>PDFile</span>
+        </Link>
+      </div>
+
+      <div>
+        <button></button>
+        <Link href="/merge">Merge</Link>
+        <Link href="/split">Split</Link>
+        <Link href="/compress">Compress</Link>
+      </div>
+
+      <div>
+        <Link href="/donate">Donate</Link>
+        <Link href="/github">Github</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
